e4135087ab98ab7f04b65a401342e4c6d95cf961
aazaf <aazafmax2@gmail.com>
2021-12-05 18:35:58 +0530

    insert dummy data for the text fields

---
+++ Aasan-ios-todo/view/OngoingTaskTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  OngoingTaskTableViewCell.swift
+//  Aasan-ios-todo
+//
+//  Created by Mohomed Aazaf on 12/5/21.
+//  Copyright © 2021 Mohomed Aazaf. All rights reserved.
+//
+
+import UIKit
+
+class OngoingTaskTableViewCell: UITableViewCell  {
+    
+    @IBOutlet weak var titleLable : UILabel!
+    @IBOutlet weak var deadlineLabel : UILabel!
+    
+    @IBAction func actionButtonTapped(_ sender : UIButton){
+        
+    }
+}
